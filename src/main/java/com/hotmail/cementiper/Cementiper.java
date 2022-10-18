d717e640f62816d624a4c3a62df297a588a17c8a
polim <layonell_@hotmail.com>
2022-10-18 00:13:06 -0500

    plantilla

--- src/main/java/com/hotmail/cementiper/Cementiper.java
+++ src/main/java/com/hotmail/cementiper/Cementiper.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
+//primera plantilla
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Cementiper.MODID)
 public class Cementiper
